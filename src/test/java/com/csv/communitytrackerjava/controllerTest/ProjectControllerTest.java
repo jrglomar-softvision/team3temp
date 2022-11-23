eee7e62dc27fdb66cc9eda576616969c2baa5a8c
jhonlyoyd <jhonlyoyd.alejo@softvision.com>
2022-11-23 18:09:04 +0800

    Unit test

--- src/test/java/com/csv/communitytrackerjava/controllerTest/ProjectControllerTest.java
+++ src/test/java/com/csv/communitytrackerjava/controllerTest/ProjectControllerTest.java
@@ -1,8 +1,10 @@
 package com.csv.communitytrackerjava.controllerTest;
 
+import com.csv.communitytrackerjava.exception.RecordNotFoundException;
 import com.csv.communitytrackerjava.model.Project;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -19,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 public class ProjectControllerTest {
-    Project magenic;
+    Project magenic, lorem;
     @MockBean
     private ProjectService projectService;
     @Autowired
@@ -30,6 +32,13 @@ public class ProjectControllerTest {
     @BeforeEach
     void setup() {
         magenic = new Project(1, "MagenicPDPBench", "MDP-101", true);
+        lorem = new Project(2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Integer vulputate laoreet lacus, id rhoncus leo ultricies vitae. Nunc et imperdiet ipsum, " +
+                "molestie porta justo. Aenean commodo porttitor posuere. Cras ac sapien ac justo lacinia egestas ac a justo. " +
+                "Nunc vulputate, turpis volutpat elementum varius, velit neque congue metus, at vulputate tellus urna commodo dolor. " +
+                "In at consectetur diam, vel molestie augue. Vivamus non fringilla diam. Duis non sem gravida, posuere est at, consequat libero. " +
+                "Quisque id rutrum velit, quis convallis libero. Quisque ac nisi quis orci sodales molestie. Etiam non diam est. " +
+                "Mauris fermentum eros ac euismod sagittis el de jojo.", "LOREM-102", true);
     }
 
     @Test
@@ -46,6 +55,22 @@ public class ProjectControllerTest {
                 .andExpect(jsonPath("$.projectCode", CoreMatchers.is(magenic.getProjectCode())))
                 .andExpect(jsonPath("$.projectDesc", CoreMatchers.is(magenic.getProjectDesc())))
                 .andExpect(jsonPath("$.type", CoreMatchers.is(magenic.getIsActive())));
+
+    }
+    @Test
+    void saveProjectWithNoDuplicates() throws Exception {
+        //Arrange
+        when(projectService.findById(anyLong()))
+                .thenReturn(lorem);
+
+        //Act
+        mockMvc.perform(post("/projects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(lorem)))
+                .andExpect(jsonPath("$.projectCode", CoreMatchers.is(lorem.getProjectCode())))
+                .andExpect(jsonPath("$.projectDesc", CoreMatchers.is(lorem.getProjectDesc())))
+                .andExpect(jsonPath("$.type", CoreMatchers.is(lorem.getIsActive())))
+                .andExpect(status().isNotAcceptable());
 
     }
 }
--- src/test/java/com/csv/communitytrackerjava/serviceTest/ProjectServiceTest.java
+++ src/test/java/com/csv/communitytrackerjava/serviceTest/ProjectServiceTest.java
@@ -1,23 +1,26 @@
 package com.csv.communitytrackerjava.serviceTest;
 
+import com.csv.communitytrackerjava.exception.RecordNotFoundException;
 import com.csv.communitytrackerjava.model.Project;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProjectServiceTest {
-    Project magenic;
+    Project magenic, lorem;
     @Mock
     private ProjectRepository projectRepository;
     @InjectMocks
@@ -27,13 +30,20 @@ public class ProjectServiceTest {
     @BeforeEach
     void setup() {
         magenic = new Project(1, "MagenicPDPBench", "MDP-101", true);
+        lorem = new Project(2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Integer vulputate laoreet lacus, id rhoncus leo ultricies vitae. Nunc et imperdiet ipsum, " +
+                "molestie porta justo. Aenean commodo porttitor posuere. Cras ac sapien ac justo lacinia egestas ac a justo. " +
+                "Nunc vulputate, turpis volutpat elementum varius, velit neque congue metus, at vulputate tellus urna commodo dolor. " +
+                "In at consectetur diam, vel molestie augue. Vivamus non fringilla diam. Duis non sem gravida, posuere est at, consequat libero. " +
+                "Quisque id rutrum velit, quis convallis libero. Quisque ac nisi quis orci sodales molestie. Etiam non diam est. " +
+                "Mauris fermentum eros ac euismod sagittis el de jojo.", "LOREM-102", true);
 
     }
 
     @Test
     public void saveProject() {
         //Arrange
-        when(projectRepository.findAll()).thenReturn(List.of(magenic));
+        when(projectRepository.save(any(Project.class))).thenReturn(magenic);
         //Act
         Project newProject = projectService.saveProject(magenic);
         //Assert
@@ -41,6 +51,50 @@ public class ProjectServiceTest {
         assertNotNull(newProject);
 
         //Newly created project id should auto generate; it should be not null
-        assertThat(newProject.getProjectCode()).isEqualTo("MDP-101");
+        assertEquals(newProject, magenic);
+    }
+
+    @Test
+    public void saveProjectWithLimitsInProjectCode() {
+        when(projectRepository.save(any(Project.class))).thenReturn(lorem);
+        //Act
+        Project newProject = projectService.saveProject(lorem);
+        //Assert
+        //Newly created project should be not null
+        assertNotNull(newProject);
+
+        //Newly created project id should auto generate; it should be not null
+        assertThat(100).isLessThanOrEqualTo(newProject.getProjectCode().length());
+        assertThrows(ResponseStatusException.class, () -> projectService.save(lorem));
+//        assertThat(newProject.getProjectCode()).isNotNull().hasSize(100);
+    }
+    @Test
+    public void saveProjectWithLimitsInProjectDesc() {
+        when(projectRepository.save(any(Project.class))).thenReturn(lorem);
+        //Act
+        Project newProject = projectService.saveProject(lorem);
+        //Assert
+        //Newly created project should be not null
+        assertNotNull(newProject);
+
+        //Newly created project id should auto generate; it should be not null
+        assertThat(100).isLessThan(newProject.getProjectCode().length());
+        assertThrows(ResponseStatusException.class, () -> projectService.save(lorem));
+//        assertThat(newProject.getProjectCode()).isNotNull().hasSize(100);
+    }
+
+    @Test
+    public void saveProjectWithNoDuplicates() {
+        when(projectRepository.findOne(anyString()))
+                .thenReturn(magenic).thenThrow(RecordNotFoundException.class);
+        //Act
+        Project newProject = projectService.saveProject(magenic);
+        //Assert
+        //Newly created project should be not null
+        assertNotNull(newProject);
+
+        //Newly created project id should auto generate; it should be not null
+        assertNotEquals(newProject,magenic);
+//        assertThat(newProject.getProjectCode()).isNotNull().hasSize(100);
     }
 }
