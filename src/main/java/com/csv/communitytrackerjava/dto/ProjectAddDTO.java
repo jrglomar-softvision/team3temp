d7cd42587254d383d4e58a002e05019f74d78dc5
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-28 18:31:23 +0800

    Fix bug for empty string

--- src/main/java/com/csv/communitytrackerjava/dto/ProjectAddDTO.java
+++ src/main/java/com/csv/communitytrackerjava/dto/ProjectAddDTO.java
@@ -1,5 +1,6 @@
 package com.csv.communitytrackerjava.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -10,10 +11,12 @@ public class ProjectAddDTO {
 
     @Size(max = 100)
     @NotEmpty
+    @NotBlank
     private String projectDesc;
 
     @Size(max = 100)
     @NotEmpty
+    @NotBlank
     private String projectCode;
 
     @NotNull
